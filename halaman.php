66f96f3cb8a8d585fa1fb2e8fa478838a7e02872
muhammadfiqri30 <111472471+muhammadfiqri30@users.noreply.github.com>
2023-08-17 15:54:28 +0700

    Add files via upload

---
+++ halaman.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="halaman.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet"
+    href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap" rel="stylesheet">
+
+    <title>Halaman</title>
+</head>
+<body>
+    
+    <header>
+        <a href="#" class="logo"><i class="ri-home-smile-2-fill"></i><span>Fhasion Room</span></a>
+        <ul class="navbar">
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#" class="active">About Us</a></li>
+            <li><a href="#" class="active">Servise</a></li>
+            <li><a href="#" class="active">Blog</a></li>
+            <li><a href="#" class="active">Contact</a></li>
+        </ul>
+
+        <div class="main">
+            <a href="signup.php" class="user"><i class="ri-user-add-line"></i>Sign-up</a>
+            <a href="login.php">Login</a>
+        </div>
+    </header>
+
+    <script src="halaman.js"></script>
+<div class="container">
+    <form action="box shadow-box">
+
+    </form>
+</div>
+</body>
+</html>
